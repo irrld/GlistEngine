36664706b61dfb8811e3dd2222a1c761a1f074f9
Remzi <Remziisci@yahoo.com>
2026-07-17 12:01:15 +0300

    Created Tween Animation System
    Seample :
     /*
     * gCanvas.h
     *
     *  Created on: May 6, 2020
     *      Author: Noyan Culum
     */
    
    #ifndef GCANVAS_H_
    #define GCANVAS_H_
    
    #include "gBaseCanvas.h"
    #include "gApp.h"
    #include "gImage.h"
    #include "gTweenAnimation.h"
    
    class gCanvas : public gBaseCanvas {
    public:
    	gCanvas(gApp* root);
    	virtual ~gCanvas();
    
    	void setup();
    	void update();
    	void draw();
    
    	void keyPressed(int key);
    	void keyReleased(int key);
    	void charPressed(unsigned int codepoint);
    	void mouseMoved(int x, int y );
    	void mouseDragged(int x, int y, int button);
    	void mousePressed(int x, int y, int button);
    	void mouseReleased(int x, int y, int button);
    	void mouseScrolled(int x, int y);
    	void mouseEntered();
    	void mouseExited();
    	void windowResized(int w, int h);
    
    	void showNotify();
    	void hideNotify();
    
    private:
    	gApp* root;
    	gImage logo;
    	gTweenAnimation anim;
    
    	float logox;
    };
    
    #endif /* GCANVAS_H_ */
    
    /*
    * gCanvas.cpp
    *
    *  Created on: May 6, 2020
    *      Author: Noyan Culum
    */
    
    #include "gCanvas.h"
    
    gCanvas::gCanvas(gApp* root) : gBaseCanvas(root) {
    	this->root = root;
    }
    
    gCanvas::~gCanvas() {
    }
    
    void gCanvas::setup() {
    	logo.loadImage("glistengine_logo.png");
    	logox = (getWidth() - logo.getWidth()) / 2;
    	anim.Set(0.0f, logox, 2.0f, &logox, gTweenAnimation::EASE_IN_BOUNCE, []() {
    		static int counter = 0;
    		counter++;
    		gLogi("gCanvas") << "Tween Animasyon : " << counter << " kere calisti";
    	});
    	anim.Start(-1);
    }
    
    void gCanvas::update() {
    	anim.Update(gApp::getAppManager()->getElapsedTime());
    }
    
    void gCanvas::draw() {
    	logo.draw(logox, (getHeight() - logo.getHeight()) / 2);
    }
    
    void gCanvas::keyPressed(int key) {
    //	gLogi("gCanvas") << "keyPressed:" << key;
    }
    
    void gCanvas::keyReleased(int key) {
    //	gLogi("gCanvas") << "keyReleased:" << key;
    }
    
    void gCanvas::charPressed(unsigned int codepoint) {
    //	gLogi("gCanvas") << "charPressed:" << gCodepointToStr(codepoint);
    }
    
    void gCanvas::mouseMoved(int x, int y) {
    //	gLogi("gCanvas") << "mouseMoved" << ", x:" << x << ", y:" << y;
    }
    
    void gCanvas::mouseDragged(int x, int y, int button) {
    //	gLogi("gCanvas") << "mouseDragged" << ", x:" << x << ", y:" << y << ", b:" << button;
    }
    
    void gCanvas::mousePressed(int x, int y, int button) {
    //	gLogi("gCanvas") << "mousePressed" << ", x:" << x << ", y:" << y << ", b:" << button;
    }
    
    void gCanvas::mouseReleased(int x, int y, int button) {
    //	gLogi("gCanvas") << "mouseReleased" << ", button:" << button;
    }
    
    void gCanvas::mouseScrolled(int x, int y) {
    //	gLogi("gCanvas") << "mouseScrolled" << ", x:" << x << ", y:" << y;
    }
    
    void gCanvas::mouseEntered() {
    
    }
    
    void gCanvas::mouseExited() {
    
    }
    
    void gCanvas::windowResized(int w, int h) {
    
    }
    
    void gCanvas::showNotify() {
    
    }
    
    void gCanvas::hideNotify() {
    
    }

---
+++ engine/animation/gTweenAnimation.cpp
@@ -0,0 +1,134 @@
+/*
+ * gTweenAnimation.cpp
+ *
+ *  Created on: 17 Jul 2026
+ *      Author: Remzi ISCI
+ */
+
+#include "gTweenAnimation.h"
+#include "gUtils.h"
+
+gTweenAnimation::gTweenAnimation() {}
+gTweenAnimation::~gTweenAnimation() {}
+
+void gTweenAnimation::Set(float startvalue, float endvalue, float duration, float *targetptr, EASE_TYPE easetype, std::function<void()> FinishCallback) {
+    this->startvalue = startvalue;
+    this->endvalue = endvalue;
+    this->duration = (duration <= 0.0f) ? 0.001f : duration; // 0'a bölme hatasýný önlemek için güvenlik sýnýrý
+    this->elapsedtime = 0.0f;
+    this->targetptr = targetptr;
+    this->easetype = easetype;
+    this->FinishCallback = FinishCallback;
+    this->isactive = false;
+    this->isfinished = false;
+    AssignEaseFunction();
+}
+void gTweenAnimation::SetEasetype(EASE_TYPE easetype) {
+    this->easetype = easetype;
+    AssignEaseFunction();
+}
+void gTweenAnimation::Update(float deltatime) {
+    if (!isactive || isfinished) return;
+
+    elapsedtime += deltatime;
+    // isfinished will be setted true when duration is reached
+    if (elapsedtime >= duration) {
+        elapsedtime = duration;
+        isfinished = true;
+    }
+
+    float t = elapsedtime / duration;
+    if (easeFunc != nullptr) {
+        t = easeFunc(t);
+    }
+
+    if (targetptr != nullptr) {
+        *targetptr = startvalue + t * (endvalue - startvalue);
+    } else {
+    	 gLogi("gTweenAnimation") << "A tween animation is working without target ptr";
+    }
+
+    if (isfinished) {
+        if (repeatcount == -1) {
+            // Sonsuz döngü modu
+            elapsedtime = 0.0f;
+            isfinished = false;
+        } else if (repeatcount > 1) {
+            // Kalan tekrar sayýsý var ise azalt ve sýfýrla
+            repeatcount--;
+            elapsedtime = 0.0f;
+            isfinished = false;
+        } else {
+            // repeatcount == 0 (Animasyon tamamen bitti)
+            isactive = false;
+        }
+        if (FinishCallback != nullptr) {
+            FinishCallback();
+        }
+    }
+}
+
+void gTweenAnimation::AssignEaseFunction() {
+    switch (easetype) {
+        case EASE_LINEAR:          easeFunc = EaseLinear; break;
+        case EASE_IN:              easeFunc = EaseIn; break;
+        case EASE_OUT:             easeFunc = EaseOut; break;
+        case EASE_IN_OUT:          easeFunc = EaseInOut; break;
+        case EASE_IN_CUBIC:        easeFunc = EaseInCubic; break;
+        case EASE_OUT_CUBIC:       easeFunc = EaseOutCubic; break;
+        case EASE_IN_OUT_CUBIC:    easeFunc = EaseInOutCubic; break;
+        case EASE_IN_QUART:        easeFunc = EaseInQuart; break;
+        case EASE_OUT_QUART:       easeFunc = EaseOutQuart; break;
+        case EASE_IN_OUT_QUART:    easeFunc = EaseInOutQuart; break;
+        case EASE_IN_QUINT:        easeFunc = EaseInQuint; break;
+        case EASE_OUT_QUINT:       easeFunc = EaseOutQuint; break;
+        case EASE_IN_OUT_QUINT:    easeFunc = EaseInOutQuint; break;
+        case EASE_IN_SINE:         easeFunc = EaseInSine; break;
+        case EASE_OUT_SINE:        easeFunc = EaseOutSine; break;
+        case EASE_IN_OUT_SINE:     easeFunc = EaseInOutSine; break;
+        case EASE_IN_EXPO:         easeFunc = EaseInExpo; break;
+        case EASE_OUT_EXPO:        easeFunc = EaseOutExpo; break;
+        case EASE_IN_OUT_EXPO:     easeFunc = EaseInOutExpo; break;
+        case EASE_IN_CIRCULAR:     easeFunc = EaseInCircular; break;
+        case EASE_OUT_CIRCULAR:    easeFunc = EaseOutCircular; break;
+        case EASE_IN_OUT_CIRCULAR: easeFunc = EaseInOutCircular; break;
+        case EASE_IN_BACK:         easeFunc = EaseInBack; break;
+        case EASE_OUT_BACK:        easeFunc = EaseOutBack; break;
+        case EASE_IN_OUT_BACK:     easeFunc = EaseInOutBack; break;
+        case EASE_IN_BOUNCE:       easeFunc = EaseInBounce; break;
+        case EASE_OUT_BOUNCE:      easeFunc = EaseOutBounce; break;
+        case EASE_IN_OUT_BOUNCE:   easeFunc = EaseInOutBounce; break;
+        default:
+            easeFunc = EaseLinear;
+            gLogi("gTweenAnimation") << "Unsupported ease type: " << easetype << ". Fallback to Linear.";
+            break;
+    }
+}
+
+void gTweenAnimation::Destroy() {}
+
+void gTweenAnimation::Reset(int repeatcount) {
+    elapsedtime = 0.0f;
+    isfinished = false;
+    this->repeatcount = repeatcount;
+    if (targetptr != nullptr) {
+        *targetptr = startvalue;
+    }
+    isactive = false;
+}
+
+void gTweenAnimation::Start(int repeatcount) {
+    isactive = true;
+    elapsedtime = 0.0f;
+    isfinished = false;
+    this->repeatcount = repeatcount;
+    if (targetptr != nullptr) {
+        *targetptr = startvalue;
+    } else {
+        gLogi("gTweenAnimation") << "Target pointer is NULL on Start!";
+    }
+}
+
+void gTweenAnimation::Stop() {
+    isactive = false;
+}
